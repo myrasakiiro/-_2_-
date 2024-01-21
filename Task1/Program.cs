eeacd78635aa025ff4901485f70400eeb6d1dc51
Viktoria <tarhovaviktoria00@gmail.com>
2024-01-21 16:29:05 +0200

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -15,15 +15,74 @@
 
 //int[] arr3 = {1,2,3,4,5};
 
-int n = 10;
-int [] arr = new int [n];
-int i = 0;
+//Создать массив и заполнить его целыми числами от 1 до N (N – произвольное
+//натуральное число). Вывести элементы массива на экран.
 
-while(i < n );
+//int n = 10;
+//int[] arr = new int[n];
+//int i = 0;
+
+//while (i < arr.Length)
+//{
+//    arr[i] = i + 1;
+///Console.Write(arr[i]);
+///Console.Write(' ');
+//   Console.Write($"{arr[i]} ");
+// i = i + 1;
+//}
+
+//Создать целочисленный массив на 10 элементов и заполнить его произвольными
+//числами. Вывести на экран чётные элементы массива
+
+//int n = 10;
+//int[] array = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+//int i = 0;
+//while (i < array.Length)
+//{
+//   if (array[i] % 2 == 0)
+//  {
+//     Console.Write($"{array[i]} ");
+//  }
+// i = i + 1;
+//}
+
+
+//Дано N гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири.
+
+int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+
+while (i < n)
 {
-    arr(i) = i + 1;
-    //Console.Write(arr[i]);
-    //Console.Write(' ');
-    Console.Write($"{arr[i]} ");
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
     i = i + 1;
+
 }
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{   
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
