02116bd3db81870f6428b400976ad3af6c89c8c8
Viktoria <tarhovaviktoria00@gmail.com>
2024-01-21 03:05:01 +0200

    add Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,16 +1,29 @@
-﻿int[] arr = new int[5];
-arr[0] = 5;
-arr[1] = 7;
-arr[2] = 5;
-arr[3] = 4;
-arr[4] = 3;
+﻿//int[] arr = new int[5];
+//arr[0] = 5;
+//arr[1] = 7;
+//arr[2] = 5;
+//arr[3] = 4;
+//arr[4] = 3;
 
-Console.WriteLine(arr[0]);
-Console.WriteLine(arr[1]);
-Console.WriteLine(arr[2]);
-Console.WriteLine(arr[3]);
-Console.WriteLine(arr[4]);
+//Console.WriteLine(arr[0]);
+//Console.WriteLine(arr[1]);
+//Console.WriteLine(arr[2]);
+//Console.WriteLine(arr[3]);
+//Console.WriteLine(arr[4]);
 
 //int[] arr2 = new int [5] {1,2,3,4,5};
 
 //int[] arr3 = {1,2,3,4,5};
+
+int n = 10;
+int [] arr = new int [n];
+int i = 0;
+
+while(i < n );
+{
+    arr(i) = i + 1;
+    //Console.Write(arr[i]);
+    //Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
